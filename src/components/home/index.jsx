41091cbae894078a52425e70ac9d23d60b5ffa64
ernicani <t.lindeker@h3hitema.fr>
2023-02-25 00:11:03 +0000

    sidebar

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import { FaFire, FaPlus, FaSignOutAlt,FaServer } from 'react-icons/fa';
+import { useParams, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
-import { Link } from 'react-router-dom';
-
-import { useTranslation } from "react-i18next";
-
-export function Home(props) {
-
-    const { t } = useTranslation();
+import './index.css';
 
 
+
+export function Home() {
 
     return (
-        <div>
-            <h1>{t('home')}</h1>
-            <Link to="/log">{t('login_button')}</Link>
+        <div className="fakeParent">  
+            <div className="parent">
+                <div>
+                    <span className="enfant">Connections</span>
+                    <SideBarIcon icon={<FaFire />} text="Home" active={true} />
+                </div>
+                <div>
+                    <span className="enfant">Add</span>
+                    <SideBarIcon icon={<FaPlus />} text="Add" />
+                </div>
+            </div>
         </div>
     );
 
+  
+
 }
+
+function SideBarIcon(props) {
+
+    const { icon, text, active, id, onClick } = props;
+
+    return (
+        <li className="squircle purple-boi">
+            {icon ? icon : text[0]}
+        </li>
+
+    );
+}
+
+
+const Divider = () => <hr className="sidebar-hr" />;
+
