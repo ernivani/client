85aee967e5984b0b7734932116306b3430b3464f
Thomas Lindeker <tlindeker0@gmail.com>
2023-02-26 07:04:21 +0000

    enlevage des logs

--- src/components/accountBox/checkAuth/index.jsx
+++ src/components/accountBox/checkAuth/index.jsx
@@ -4,14 +4,12 @@ import io from 'socket.io-client';
 export default function checkAuth(link) {
     const token = localStorage.getItem('token');
 
-    console.log(link);
     if (token) {
       const socket = io.connect('http://213.32.89.28:5000');
       socket.emit('auth', token);
       socket.on('authResponse', (data) => {
-        console.log(data)
         if (data.status == 'success') {
-            
+          // user is authenticated
             if (link === '/log') {
                 window.location.href = '/channels/@me';
             }
