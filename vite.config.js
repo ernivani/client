35580678a3cee09a43ab31cc7b1dea92e2642a06
Thomas Lindeker <tlindeker@gmail.com>
2023-04-06 11:15:02 +0000

    config

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        app: './index.html',
-      },
-      port: 8080,
-    },
-  },
-  dev : {
-    port: 8080,
-    rollupOptions: {
-      input: {
-        app: './index.html',
-      },
-    },
-  },
 })
