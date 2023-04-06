9e19ee18e1f51405437141bbaf58ae9d27a8f821
Thomas Lindeker <tlindeker@gmail.com>
2023-04-06 13:30:09 +0000

    popup for the icon names

--- src/components/users/sideBar/index.jsx
+++ src/components/users/sideBar/index.jsx
@@ -92,7 +92,7 @@ function SideBarIcon(props) {
 
     const handleMouseEnterInternal = (event) => {
         const pos = event.currentTarget.getBoundingClientRect().top + 5;
-
+    
         const enfant = (
             <Enfant
                 style={{
@@ -103,31 +103,25 @@ function SideBarIcon(props) {
                     height: "fit-content",
                     zIndex: "999",
                     opacity: "0",
-                    transition: "opacity 0.5s ease-in-out", // ajout de la transition
+                    transition: "opacity 0.5s ease-in-out",
+                    willChange: "opacity",
                 }}
             >
                 {text}
             </Enfant>
         );
-
+    
         const container = document.createElement("div");
         container.setAttribute("id", "popup");
         document.querySelector(".user").appendChild(container);
-
-        // Mettre l'opacité à 1 avec une transition de 0.5s
-        
-        // Fonction pour augmenter l'opacité petit à petit
-        let opacity = 0;
-        const intervalId = setInterval(() => {
-            opacity += 0.1;
-            enfant.props.style.opacity = opacity;
-            createRoot(container).render(enfant);
-            if (opacity >= 1) {
-                clearInterval(intervalId);
-            }
-        }, 15);
+    
+        // Définir l'opacité à 1 avec une transition
+        requestAnimationFrame(() => {
+            enfant.props.style.opacity = 1;
+        });
+        createRoot(container).render(enfant);
     };
-
+    
     const handleMouseLeave = () => {
         const div = document.getElementById("popup");
         if (div) {
