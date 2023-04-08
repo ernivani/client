21c984f421cc5fe29ffb6ec34728b442f37d75d9
Thomas Lindeker <tlindeker@gmail.com>
2023-04-08 17:25:12 +0000

    error handle on compoesent

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -19,28 +19,41 @@ export function LoginForm() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setErrorMessage] = useState(null);
+
+    const [error_email, setErrorEmail] = useState("");
+    const [error_password, setErrorPassword] = useState("");
+
+    const verify = (e) => {
+        e.preventDefault();
+        setErrorEmail("");
+        setErrorPassword("");
+        if (email === "" || !email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+            setErrorEmail(t("email_required"));
+            return;
+        }
+        if (password === "") {
+            setErrorPassword(t("password_required"));
+            return;
+        }
+        import("./LogSend/login")
+            .then((module) => {
+                module.loginSend(email,password, setErrorEmail, setErrorPassword);
+            })
+            .catch((error) => {
+                setErrorEmail(error);
+                setErrorPassword(error);
+            });
+    };
+
+                
 
     return (
         <BoxContainer>
-            <FormContainer
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    import("./LogSend/login")
-                        .then((module) => {
-                            module.loginSend(e, setErrorMessage);
-                        })
-                        .catch((error) => {
-                            alert(error);
-                        });
-                }}
-                // prevent default
-            >
+            <FormContainer>
                 <LabelInput>
                     {t("email_label")}
-                    {/* if error place it else place * */}
-                    {error ? (
-                        <ImportantSpan>- {error}</ImportantSpan>
+                    {error_email ? (
+                        <ImportantSpan>- {error_email}</ImportantSpan>
                     ) : (
                         <ImportantSpan>*</ImportantSpan>
                     )}
@@ -55,12 +68,11 @@ export function LoginForm() {
                     maxLength="999"
                     onChange={(e) => setEmail(e.target.value)}
                     value={email}
-                    required
                 />
                 <LabelInput>
                     {t("password_label")}
-                    {error ? (
-                        <ImportantSpan>- {error}</ImportantSpan>
+                    {error_password ? (
+                        <ImportantSpan>- {error_password}</ImportantSpan>
                     ) : (
                         <ImportantSpan>*</ImportantSpan>
                     )}
@@ -75,20 +87,20 @@ export function LoginForm() {
                     maxLength="999"
                     onChange={(e) => setPassword(e.target.value)}
                     value={password}
-                    required
                 />
                 <Marginer direction="vertical" margin={10} />
                 <BoldLink onClick={
                     (e) => {
+                        setErrorEmail("");
                         e.preventDefault();
                         if (email === "") {
-                            setErrorMessage(t("email_required"));
+                            setErrorEmail(t("email_required"));
                             return;
                         }
                         import("./LogSend/resetPassword")
                             .then((module) => {
                                 console.log(email)
-                                module.resetPasswordSend(email, setErrorMessage);
+                                module.resetPasswordSend(email, setErrorEmail);
                             }
                             )
                             .catch((error) => {
@@ -97,7 +109,7 @@ export function LoginForm() {
                     }
                 }>{t("forgot_password")}</BoldLink>
                 <Marginer direction="vertical" margin="1.6em" />
-                <SubmitButton type="submit">{t("login_button")}</SubmitButton>
+                <SubmitButton onClick={(e)=>verify(e)}>{t("login_button")}</SubmitButton>
             </FormContainer>
             <Marginer direction="vertical" margin="1em" />
             <InfoLink>
