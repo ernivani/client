1834fd0707363341ba2d92fbf22d6828fd8ed687
ernivani <tlindeker0@gmail.com>
2023-04-12 13:49:33 +0000

    channels de serveur

--- src/components/users/Content/ChannelBar/ServerChannel/index.jsx
+++ src/components/users/Content/ChannelBar/ServerChannel/index.jsx
@@ -1,28 +1,71 @@
-import styled from "styled-components";
+import { FaCaretDown } from "react-icons/fa";
+import {
+  ChannelsList,
+  ChannelsListItem,
+  ChannelsListLink,
+  ChannelName,
+  ServerName
+} from "../css";
 
-import { 
-    ChannelsList, 
-    ChannelsListItem, 
-    ChannelsListLink, 
-    ChannelName,
-    ServerName
- } from "../css";
+import { useEffect, useState } from "react";
 
+export default function ServerChannel({ socket, serverId, channelId }) {
+  const userCache = localStorage.getItem("userCache");
+  const userId = JSON.parse(userCache).userId;
+  socket = socket.socket;
 
-export default function ServerChannel({ socket, serverId, channelId, userId }) {
-    
-    return (
-        <>
-            <ServerName>Server Name</ServerName>
-            <ChannelsList>
-                <ChannelsListItem>
-                    <ChannelsListLink to={`/channels/${serverId}/${channelId}`}>
-                        <ChannelName>#general</ChannelName>
-                    </ChannelsListLink>
-                </ChannelsListItem>
-            </ChannelsList>
-        </>
-        
-        
-    );
+  const [channels, setChannels] = useState([]);
+  socket.emit("get-channel-by-server", { serverId, userId });
+
+  useEffect(() => {
+    socket.on("channel-list", (data) => {
+      setChannels(data);
+    });
+  }, [socket]);
+
+  return (
+    <>
+      <ServerName>
+        <span style={{ margin: "auto 0" }}>Server Name</span>
+        <FaCaretDown style={{ fontSize: "12px", margin: "auto 0" }} />
+      </ServerName>
+      {channels.map((channel) => {
+        if (channel.type === "text") {
+          return (
+            <ChannelsListItem key={channel.id}>
+              <ChannelsListLink to={`/channels/${serverId}/${channel.id}`}>
+                <ChannelName>#{channel.name}</ChannelName>
+              </ChannelsListLink>
+            </ChannelsListItem>
+          );
+        } else if (channel.type === "voice") {
+          // Render voice channel
+        } else if (channel.type === "category") {
+          return (
+            <div key={channel.id}>
+              {/* Render category title */}
+              <h3>{channel.name}</h3>
+              <ChannelsList>
+                {/* {channel.channels.map((subChannel) => {
+                  if (subChannel.type === "text") {
+                    return (
+                      <ChannelsListItem key={subChannel.id}>
+                        <ChannelsListLink
+                          to={`/channels/${serverId}/${subChannel.id}`}
+                        >
+                          <ChannelName>#{subChannel.name}</ChannelName>
+                        </ChannelsListLink>
+                      </ChannelsListItem>
+                    );
+                  } else if (subChannel.type === "voice") {
+                    // Render voice channel
+                  }
+                })} */}
+              </ChannelsList>
+            </div>
+          );
+        }
+      })}
+    </>
+  );
 }
