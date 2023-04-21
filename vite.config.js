39116c2419083118a1b96460ef5b2d4490e502b3
ernivani <tlindeker0@gmail.com>
2023-04-21 14:06:10 +0000

    update api

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // todo: allow fetch from status.impin.fr
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.impin.fr',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  } 
+  
 })
+
