ee844e54c046718f3a68d4a91ed65b19f88501fc
ernivani <tlindeker0@gmail.com>
2023-05-15 09:47:38 +0000

    prettier

--- src/components/users/topBar/index.jsx
+++ src/components/users/topBar/index.jsx
@@ -5,100 +5,100 @@ import React, { useCallback, useEffect } from "react";
 import { TopbarNav, Serv, Squircle, Divider } from "./css";
 
 export default function TopBar({
-	serverList,
-	userId,
-	setIsVisible,
-	addServer,
-	disconnect,
-	navigate
+    serverList,
+    userId,
+    setIsVisible,
+    addServer,
+    disconnect,
+    navigate,
 }) {
-	const { id } = useParams();
+    const { id } = useParams();
 
-	// serverList doit etre multipliée par 10 pour que le scroll fonctionne
+    // serverList doit etre multipliée par 10 pour que le scroll fonctionne
 
-	useEffect(() => {
-		const topbarNav = document.querySelector(".topbar_nav");
+    useEffect(() => {
+        const topbarNav = document.querySelector(".topbar_nav");
 
-		function handleWheelEvent(e) {
-			e.preventDefault();
-			topbarNav.scrollLeft += e.deltaY;
-		}
+        function handleWheelEvent(e) {
+            e.preventDefault();
+            topbarNav.scrollLeft += e.deltaY;
+        }
 
-		topbarNav.addEventListener("wheel", handleWheelEvent);
+        topbarNav.addEventListener("wheel", handleWheelEvent);
 
-		return () => {
-			topbarNav.removeEventListener("wheel", handleWheelEvent);
-		};
-	}, []);
+        return () => {
+            topbarNav.removeEventListener("wheel", handleWheelEvent);
+        };
+    }, []);
 
-	return (
-		<TopbarNav className="topbar_nav">
-			<SideBarIcon
-				id="@me"
-				nav={navigate}
-				icon={<FaFire />}
-				text="Messages privé"
-				active={id === "@me"}
-				hoverColor="var(--color-blurple)"
-			/>
-			<Divider />
+    return (
+        <TopbarNav className="topbar_nav">
+            <SideBarIcon
+                id="@me"
+                nav={navigate}
+                icon={<FaFire />}
+                text="Messages privé"
+                active={id === "@me"}
+                hoverColor="var(--color-blurple)"
+            />
+            <Divider />
 
-			{serverList.map(({ id: serverId, name: server_name, cid: cid }) => (
-				<SideBarIcon
-					key={serverId}
-					nav={navigate}
-					id={serverId}
-					cid={cid}
-					icon={<FaServer />}
-					text={server_name}
-					active={id == serverId}
-					hoverColor="var(--color-yell-bubble)"
-				/>
-			))}
-			<Divider />
-			<SideBarIcon
-				id="addServer"
-				nav={navigate}
-				icon={<FaPlus />}
-				text="Créer un serveur"
-				active={false}
-				hoverColor="var(--color-green-bubble)"
-				onClick={addServer}
-			/>
-			<SideBarIcon
-				id="disconnect"
-				nav={navigate}
-				icon={<FaSignOutAlt />}
-				text="Déconnexion"
-				active={false}
-				hoverColor="var(--color-red-bubble)"
-				onClick={disconnect}
-			/>
-		</TopbarNav>
-	);
+            {serverList.map(({ id: serverId, name: server_name, cid: cid }) => (
+                <SideBarIcon
+                    key={serverId}
+                    nav={navigate}
+                    id={serverId}
+                    cid={cid}
+                    icon={<FaServer />}
+                    text={server_name}
+                    active={id == serverId}
+                    hoverColor="var(--color-yell-bubble)"
+                />
+            ))}
+            <Divider />
+            <SideBarIcon
+                id="addServer"
+                nav={navigate}
+                icon={<FaPlus />}
+                text="Créer un serveur"
+                active={false}
+                hoverColor="var(--color-green-bubble)"
+                onClick={addServer}
+            />
+            <SideBarIcon
+                id="disconnect"
+                nav={navigate}
+                icon={<FaSignOutAlt />}
+                text="Déconnexion"
+                active={false}
+                hoverColor="var(--color-red-bubble)"
+                onClick={disconnect}
+            />
+        </TopbarNav>
+    );
 }
 
 function SideBarIcon(props) {
-	const { icon, text, active, hoverColor, id, cid, nav, onClick } = props;
+    const { icon, text, active, hoverColor, id, cid, nav, onClick } = props;
 
-	let pathModif = "";
-	if (id !== "addServer" && id !== "disconnect") {
-		pathModif = id === "@me" ? `/channels/${id}` : `/channels/${id}/${cid}`;
-	}
+    let pathModif = "";
+    if (id !== "addServer" && id !== "disconnect") {
+        pathModif = id === "@me" ? `/channels/${id}` : `/channels/${id}/${cid}`;
+    }
 
-	let handleClick = onClick ? onClick : () => nav(pathModif);
+    let handleClick = onClick ? onClick : () => nav(pathModif);
 
-	return (
-		<Serv>
-			<Squircle
-				a={hoverColor}
-				onClick={handleClick}
-				id={id}
-				b={active ? 1 : 0}
-				aria-label="Server"
-			>
-				{icon ? icon : text[0]}
-			</Squircle>
-		</Serv>
-	);
+    return (
+        <Serv>
+            <Squircle
+                a={hoverColor}
+                onClick={handleClick}
+                id={id}
+                b={active ? 1 : 0}
+                aria-label={text}
+            >
+                {icon ? icon : text[0]}
+            </Squircle>
+        </Serv>
+    );
 }
